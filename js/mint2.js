0764e4ce36a993ae2356c4da861df8b53b298053
zalam003 <zaki.alam@gmail.com>
2023-12-30 11:36:08 -0500

    add mint function

--- js/mint2.js
+++ js/mint2.js
@@ -1,4 +1,36 @@
-// Wrap the code in an asynchronous function
+// Define the mintNFT function for minting logic
+async function mintNFT() {
+    try {
+        // Check if ethereum is available globally
+        if (typeof ethereum !== 'undefined') {
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            const networkId = await ethereum.request({ method: 'net_version' });
+
+            if (networkId === '49797') {
+                const contractAddress = '0x0b21b455850dfAE1577A605Efb5F61E093bC8B61';
+                const mintTx = {
+                    from: accounts[0],
+                    to: contractAddress,
+                };
+
+                try {
+                    const transactionHash = await ethereum.request({ method: 'eth_sendTransaction', params: [mintTx] });
+                    console.log('Transaction Hash:', transactionHash);
+                } catch (error) {
+                    console.error('Transaction Error:', error);
+                }
+            } else {
+                console.error('Please connect to the Energi testnet in MetaMask.');
+            }
+        } else {
+            console.error('MetaMask is not installed.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Wrap the minting function in an asynchronous function
 async function initMinting() {
     try {
         // Check if ethereum is available globally
@@ -6,25 +38,8 @@ async function initMinting() {
             document.getElementById('mintButton').addEventListener('click', async function (event) {
                 event.preventDefault(); // Prevent default behavior of the anchor tag
 
-                const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-                const networkId = await ethereum.request({ method: 'net_version' });
-
-                if (networkId === '49797') {
-                    const contractAddress = '0x0b21b455850dfAE1577A605Efb5F61E093bC8B61';
-                    const mintTx = {
-                        from: accounts[0],
-                        to: contractAddress,
-                    };
-
-                    try {
-                        const transactionHash = await ethereum.request({ method: 'eth_sendTransaction', params: [mintTx] });
-                        console.log('Transaction Hash:', transactionHash);
-                    } catch (error) {
-                        console.error('Transaction Error:', error);
-                    }
-                } else {
-                    console.error('Please connect to the Energi testnet in MetaMask.');
-                }
+                // Call the mintNFT function when the button is clicked
+                await mintNFT();
             });
         } else {
             console.error('MetaMask is not installed.');
@@ -34,5 +49,4 @@ async function initMinting() {
     }
 }
 
-// Call the asynchronous function to initiate minting
-initMinting();
+// Do not call the asynchronous function here
