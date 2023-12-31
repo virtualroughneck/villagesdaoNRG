6d08f7aa8a3193219eeb22ef15203f84637d173f
zalam003 <zaki.alam@gmail.com>
2023-12-31 17:44:26 -0500

    remove ref of ethers library

--- js/mint2.js
+++ js/mint2.js
@@ -44,42 +44,42 @@ async function getProvider() {
         {"type":"function","stateMutability":"nonpayable","outputs":[],"name":"withdrawPayments","inputs":[{"type":"address","name":"payee","internalType":"address payable"}]}
       ];
   
-    const provider = await getProvider();
-    const contract = new ethers.Contract(contractAddress, contractABI, provider);
-    return contract;
-  }
-  
-  async function mintNFT() {
-    try {
-      // Wait for MetaMask connection
-      await ethereum.request({ method: 'eth_requestAccounts' });
-  
-      // Ensure the connected network is 49797 (testnet)
-      const networkId = await ethereum.request({ method: 'net_version' });
-      if (networkId !== '49797') {
-        console.error('Please connect to the Energi testnet in MetaMask.');
-        return;
-      }
-  
-      // Use MetaMask provider directly
-      const provider = ethereum;
-      const signerAddress = (await provider.request({ method: 'eth_accounts' }))[0];
-  
-      const contract = await getContract();
-      const mintPriceInWei = await contract.MINT_PRICE();
-  
-      const transactionResponse = await contract.mintTo(signerAddress, {
-        gasLimit: 500_000,
-        gasPrice: '120000000000', // 120 GWei
-        value: mintPriceInWei,
-      });
-  
-      console.log(`Transaction Hash: ${transactionResponse.hash}`);
-    } catch (error) {
-      console.error('Error:', error);
+      const provider = await getProvider();
+      const contract = new window.ethereum.Contract(contractABI, contractAddress);
+      return contract;
     }
-  }
-  
-  // Example usage:
-  mintNFT();
-  
+    
+    async function mintNFT() {
+      try {
+        // Wait for MetaMask connection
+        await ethereum.request({ method: 'eth_requestAccounts' });
+    
+        // Ensure the connected network is 49797 (testnet)
+        const networkId = await ethereum.request({ method: 'net_version' });
+        if (networkId !== '49797') {
+          console.error('Please connect to the Energi testnet in MetaMask.');
+          return;
+        }
+    
+        // Use MetaMask provider directly
+        const provider = ethereum;
+        const signerAddress = (await provider.request({ method: 'eth_accounts' }))[0];
+    
+        const contract = await getContract();
+        const mintPriceInWei = await contract.methods.MINT_PRICE().call();
+    
+        const transactionResponse = await contract.methods.mintTo(signerAddress).send({
+          from: signerAddress,
+          gas: '500000',
+          gasPrice: '120000000000', // 120 GWei
+          value: mintPriceInWei,
+        });
+    
+        console.log(`Transaction Hash: ${transactionResponse.transactionHash}`);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+    
+    // Example usage:
+    mintNFT();
